027135dcf650a701662c5cf7ea23180337fed3c7
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 02:50:29 +0900

    ユーザー一覧画面にデータベースから取得した情報を表示

--- src/main/java/com/example/its/domain/auth/UserRepository.java
+++ src/main/java/com/example/its/domain/auth/UserRepository.java
@@ -3,10 +3,14 @@ package com.example.its.domain.auth;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface UserRepository {
     @Select("SELECT * FROM users WHERE username = #{username}")
     Optional<User> findByUsername(String username);
+
+    @Select("SELECT * FROM users")
+    List<User> findAll();
 }
