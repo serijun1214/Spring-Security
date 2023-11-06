e6eb81be25eec6c6ec6f5279d301b62b9957576a
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 04:12:45 +0900

    パスワードの長さを確かめるバリデーションを実装

--- src/main/java/com/example/its/web/user/UserForm.java
+++ src/main/java/com/example/its/web/user/UserForm.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +15,6 @@ public class UserForm {
     private String username;
 
     @NotBlank
+    @Size(min = 12, max = 128)
     private String password;
 }
