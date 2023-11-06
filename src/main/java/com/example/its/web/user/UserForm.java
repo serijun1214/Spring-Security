28f1db89859f023fc09308050125b25e46b45fcb
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 03:16:52 +0900

    ユーザー作成画面でハンドラーメソッドにバリデーションの処理を追加

--- src/main/java/com/example/its/web/user/UserForm.java
+++ src/main/java/com/example/its/web/user/UserForm.java
@@ -3,9 +3,13 @@ package com.example.its.web.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 public class UserForm {
+    @NotBlank
     private String username;
+    @NotBlank
     private String password;
 }
