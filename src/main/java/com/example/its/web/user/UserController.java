8460b8ef31706c704e5904b936e3691b69f66eb8
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 03:05:59 +0900

    ユーザー作成のハンドラーメソッドを実装

--- src/main/java/com/example/its/web/user/UserController.java
+++ src/main/java/com/example/its/web/user/UserController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collections;
@@ -27,4 +28,8 @@ public class UserController {
         return "users/creationForm";
     }
 
+    @PostMapping
+    public String create() {
+        return "redirect:/users";
+    }
 }
