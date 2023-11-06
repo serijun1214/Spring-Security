ea30814fd3fe0e5b716109f977baa0d33a392e87
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 04:38:10 +0900

    データベースから権限情報を取得

--- src/main/java/com/example/its/domain/auth/User.java
+++ src/main/java/com/example/its/domain/auth/User.java
@@ -8,4 +8,10 @@ import lombok.Data;
 public class User {
     private String username;
     private String password;
+    private Authority authority;
+
+    public enum Authority {
+        USER,
+        ADMIN
+    }
 }
