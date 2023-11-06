a67a9f4b199b9fc6765b08d881243ced2505b2de
serijun1214 <junki.seri@rakus-partners.co.jp>
2023-11-07 04:23:33 +0900

    エンコードしたパスワードを初期データとして投入

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,6 @@ insert into issues (summary, description) values ('バグA', 'バグがありま
 insert into issues (summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
 insert into issues (summary, description) values ('画面Cが遅い', '早くしてほしいです');
 
-insert into users (username, password) values ('tom', 'password');
-insert into users (username, password) values ('bob', 'password');
+-- password1234
+insert into users (username, password) values ('tom', '606e7cdd4c969d8d7a5efe5478f9a9c4d920e340ad62accfdeb5525fd641703c17bf40f9ba18c204');
+insert into users (username, password) values ('bob', '6fa4018ecd17c90ba0d0b38cf7916661c1e532dda8a3949ee5a1e06fd311ae50fa3b66e9e7415ecd');
